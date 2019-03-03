ad22bbbf42136c0d193b5c1fbec3933574b93afd
Voxylu <hello@voxylu.xyz>
2019-03-03 12:35:43 +0100

    Making exrpess working

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -6,6 +6,10 @@ import { SpaceRoom } from './SpaceRoom'
 const app = express()
 
 app.use(express.static('../server'))
+app.get('/hello', (req, res) => {
+  console.log('they want me to say hello')
+  res.json({ message: 'hello' })
+})
 
 const server = http.createServer()
 const gameServer = new Server({
@@ -15,8 +19,12 @@ const gameServer = new Server({
 gameServer.register('space', SpaceRoom)
 
 const port = process.env.PORT ? parseInt(process.env.PORT) : 4000
-const host = process.env.ENDPOINT || undefined
+// const host = process.env.ENDPOINT || undefined
 
-gameServer.listen(port, host, undefined, () => {
-  console.log(`Server started on http://${host}:${port}`)
-})
+gameServer.listen(port)
+
+console.log(`Server started on port ${port}`)
+
+// gameServer.listen(port, host, undefined, () => {
+//   console.log(`Server started on http://${host}:${port}`)
+// })
