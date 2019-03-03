37ddb2d2b0e35a05a92093097eafda6d116705ad
Voxylu <hello@voxylu.xyz>
2019-03-03 12:43:10 +0100

    trying to express

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,23 +5,25 @@ import { SpaceRoom } from './SpaceRoom'
 
 const app = express()
 
-app.use(express.static('../server'))
+app.use(express.static('../client'))
 app.get('/hello', (req, res) => {
   console.log('they want me to say hello')
   res.json({ message: 'hello' })
 })
 
 const server = http.createServer()
-const gameServer = new Server({
-  server,
-})
 
-gameServer.register('space', SpaceRoom)
+// const gameServer = new Server({
+//   server,
+// })
+
+// gameServer.register('space', SpaceRoom)
 
 const port = process.env.PORT ? parseInt(process.env.PORT) : 4000
 // const host = process.env.ENDPOINT || undefined
 
-gameServer.listen(port)
+server.listen(port)
+// gameServer.listen(port)
 
 console.log(`Server started on port ${port}`)
 
