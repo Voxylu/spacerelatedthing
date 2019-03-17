439d272bb9cf4896348864f39ac30ec6d1318e13
Voxylu <hello@voxylu.xyz>
2019-03-17 19:52:48 +0100

    Pour manu

--- packages/server/src/Ship/CreateShip.ts
+++ packages/server/src/Ship/CreateShip.ts
@@ -43,6 +43,23 @@ export const createShip = (ops: CreateShipOptions) => {
       new EnergyElement({ dx: 0, dy: -scale, shipId: ops.id }),
       new EnergyElement({ dx: 0, dy: -scale * 2, shipId: ops.id }),
       new EnergyElement({ dx: 0, dy: -scale * 3, shipId: ops.id }),
+      new EnergyElement({ dx: 0, dy: scale, shipId: ops.id }),
+      new EnergyElement({ dx: 0, dy: scale * 2, shipId: ops.id }),
+      new EnergyElement({ dx: 0, dy: scale * 3, shipId: ops.id }),
+      new CannonElement({
+        dx: scale,
+        dy: -scale * 3,
+        orientation: TriangleOrientation.Right,
+        projectileManager: ops.projectileManage,
+        shipId: ops.id,
+      }),
+      new CannonElement({
+        dx: scale,
+        dy: scale * 3,
+        orientation: TriangleOrientation.Right,
+        projectileManager: ops.projectileManage,
+        shipId: ops.id,
+      }),
       // new EnergyElement({ dx: -scale, dy: scale, shipId: ops.id }),
     ],
   }
